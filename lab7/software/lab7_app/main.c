ca35e44174e29681394fa505c7289338c5e47fef
AllenHeartcore <ziyuanc3@illinois.edu>
2023-04-01 15:07:27 -0500

    Minor fixes: ROM content, test program

--- lab7/software/lab7_app/main.c
+++ lab7/software/lab7_app/main.c
@@ -1,5 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vga_text.h"
+
 int main() {
-	printf("Starting VGA test\n\r");
+	printf("Starting VGA test\n");
 	while (1)
 		textVGATest();
 	return 0;
